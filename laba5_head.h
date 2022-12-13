9faddac8393eff5733eaa1a198dd0b1714858867
serejek2004 <serejek2004@gmail.com>
2022-12-13 22:42:12 +0200

    Upload laba5_head.h

---
+++ laba5_head.h
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+class Candidate
+{
+public:
+    string name_candidate;
+    float number_votes, summary_votes;
+    Candidate()
+    {
+    }
+    Candidate(string name_candidate, float number_votes)
+    {
+        this->name_candidate = name_candidate;
+        this->number_votes = number_votes;
+    }
+    void print_data()
+    {
+        cout << endl
+             << "Name candidate - " << name_candidate << " || ";
+        cout << "Number votes - " << number_votes << " || ";
+    }
+    void get_summary_votes(float summary_votes)
+    {
+        this->summary_votes = summary_votes;
+    }
+    void get_percent_points()
+    {
+        float percent_points;
+        percent_points = 0;
+        percent_points = (number_votes / summary_votes) * 100;
+        cout << "Percent votes " << percent_points << " %" << endl;
+    }
+};
+ostream &operator<<(ostream &os, const Candidate &other)
+{
+    os << other.name_candidate << other.number_votes << endl;
+    return os;
+}
+istream &operator>>(istream &is, Candidate &other)
+{
+    is >> other.name_candidate >> other.number_votes;
+    return is;
+}
+class Elections : public Candidate
+{
+private:
+    string name_winner;
+    int number_votes_winner;
+
+public:
+    void candidate_winner(string names_candidates[5], int number_votes[5])
+    {
+        number_votes_winner = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            if (number_votes[i] > number_votes_winner)
+            {
+                number_votes_winner = number_votes[i];
+                name_winner = names_candidates[i];
+            }
+        }
+        cout << "\n\n\n"
+             << name_winner << " won by scoring - " << number_votes_winner << " votes!!!\n\n\n";
+    }
+};
